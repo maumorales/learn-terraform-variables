46510b728a5d9b70d74943ca6e0c15468f9253dd
maumorales <mau.morales@gmail.com>
2024-05-06 21:18:04 -0600

    configure use of tfe cloud

--- terraform.tf
+++ terraform.tf
@@ -2,21 +2,20 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
-  /* Uncomment this block to use Terraform Cloud for this tutorial
   cloud {
-      organization = "organization-name"
+      organization = "QuantumSentinel"
       workspaces {
         name = "learn-terraform-variables"
       }
   }
-  */
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.15.0"
     }
   }
+
   required_version = "~> 1.2.0"
 }
 
